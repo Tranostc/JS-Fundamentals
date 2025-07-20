ae21d5997ea8040ed9f7fc5f309573a8927cb86c
Tranos Taruvinga <147081112+Tranostc@users.noreply.github.com>
2025-07-20 20:50:14 +0200

    Update 1-multi_language.js

--- 1-multi_language.js
+++ 1-multi_language.js
@@ -1,9 +1,3 @@
-const messages = ["C is fun",
-  "Python is cool",
-  "JavaScript is amazing"];
-
-function printMessages(msgArray) {
-  for (const message of msgArray) {
-    console.log(message);
-  }
-}
+console.log("C is fun");
+console.log("Python is cool");
+console.log("JavaScript is amazing");
