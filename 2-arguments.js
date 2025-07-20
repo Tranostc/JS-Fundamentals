59563d998ef1270fdb51a61d2ee11ff587414345
Tranos Taruvinga <147081112+Tranostc@users.noreply.github.com>
2025-07-20 20:47:11 +0200

    Update 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,10 @@
-const args = process.argv.slice(2);
-if (args.length === 0) 
-{console.log("No argument")}
-else if (args.length === 1)
-    {console.log("Argument found");}
-    else {console.log("Argument found");}
-    
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+if (arg1 === undefined) {
+    console.log("No argument");
+} else if (arg2 === undefined) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+}
