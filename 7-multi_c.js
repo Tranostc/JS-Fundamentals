118f2a50d3393281b088dc36c2656dea17fbc9de
Tranos Taruvinga <147081112+Tranostc@users.noreply.github.com>
2025-07-20 20:36:21 +0200

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,19 +1,8 @@
-const x = parseInt(process.argv[2]);
-
-for (let i = 0; i < 2; i++) {
-    console.log("C is fun");
-}
-
-for (let i = 0; i < 5; i++) {
-    console.log("C is fun");
-}
-
-if (isNaN(x) || x < 0) {
+const x = parseInt(process.argv[2], 10);
+if (isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
-    let i = 0;
-    while (i < x) {
+    for (let i = 0; i < x; i++) {
         console.log("C is fun");
-        i++;
     }
 }
